3fe267e7dd37381fe51257836cd2bebbf16e6ba5
SyaqinaOctavia <syaqinaoctavia@gmail.com>
2026-04-12 21:11:07 +0700

    feat: added hpp header files for Board

---
+++ include/models/Board.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+#include "Tile.hpp"
+
+class Board {
+private:
+    std::vector<Tile*> tiles;
+    std::map<std::string, Tile*> tileMap;
+    int totalTiles;
+
+public:
+    int getTileCount();
+    Tile* getTile(int index);
+    Tile* getTileByCode(std::string code);
+    int getNextTileIndex(int current, int steps);
+    int findNearestStation(int fromIndex);
+    int getJailIndex();
+    int getStartTileIndex();
+    void setTile(Tile* tile, int idx);
+};
