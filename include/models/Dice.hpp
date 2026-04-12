56c82dfe1ce13930c775a56e5b4f506e81c15422
SyaqinaOctavia <syaqinaoctavia@gmail.com>
2026-04-12 21:28:15 +0700

    feat: added hpp header files for Dice

---
+++ include/models/Dice.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <functional>
+
+class Dice{
+private:
+    int num1;
+    int num2;
+    std::function<int(int, int)> randFunc;
+
+public:
+    Dice();
+    explicit Dice(std::function<int(int, int)> randFunc);
+
+    void roll();
+    void setManual(int x, int y);
+
+    int  getTotal() const;
+    bool isDouble() const;
+    int  getDie1() const;
+    int  getDie2() const;
+};
